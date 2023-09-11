eb8a0255d45b5d37031e939e5c50f62f6aa3034c
Guilhlm <141831938+Guilhlm@users.noreply.github.com>
2023-09-11 08:59:45 -0300

    Add files via upload

---
+++ tipo de laços/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipo_de_laços
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // do { codigo } while ( condiçao )
+
+            int menu;
+
+            Console.WriteLine("1- do while \n 2- for \n3- switch \n4- array \n escolha uma das opçoes");
+            menu = int.Parse(Console.ReadLine());
+            
+            if (menu == 1)
+            {
+                int contador = 4;
+
+                do
+                {
+                    Console.WriteLine("Do While" + contador);
+                    contador++;
+
+                }while (contador <= 3);
+
+
+
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
